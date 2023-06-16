26d91827eaaa4d8d7e70282f3403dbb4b18a06b5
Ashes Maharjan <ashes.maharjan@infodevelopers.local>
2023-06-16 13:04:23 +0545

    smartcontact

--- src/main/java/com/start/controller/UserController.java
+++ src/main/java/com/start/controller/UserController.java
@@ -1,7 +1,12 @@
 package com.start.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.start.entites.Contact;
 
 @Controller
 @RequestMapping("/user")
@@ -12,4 +17,16 @@ public class UserController {
 	{
 		return "user_dashboard";
 	}
+    
+    
+ 
+    //open add form handler
+    @GetMapping("/addcontact")
+    public String openAddContactForm(Model model)
+    {
+    	 model.addAttribute("title","Add Contact");
+    	 model.addAttribute("contact", new Contact());
+    	
+    	return "addcontact";
+    }
 }
